81e46a506a802aec5992e70ff4c709e41dbef88e
danpl <danielpec@outlook.com>
2021-04-21 21:17:12 -0300

    add ps general graphs

--- trajs/functions/geohmount_plots.py
+++ trajs/functions/geohmount_plots.py
@@ -1,11 +1,14 @@
 import plotly.express as px
 
-def wind_rose(dataframe, kind='vel', title='<b>Distribuição das massas de ar resultantes em chuva no Soberbo</b>', 
+def wind_rose(dataframe, kind='vel', location='sb', title='<b>Distribuição das massas de ar resultantes em chuva no Soberbo</b>', 
              legend_title='<b>Velocidade</b>', tickvals=[4, 6, 8, 10, 12], showticklabels=True,
              color='velocidade_cat', color_sequence= px.colors.sequential.Plasma_r, font_color=None, template='plotly'):
         
     direcoes = ("N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW")
-    medidas = ('0-5 km/h', '5-10 km/h', '10-15 km/h', '15-20 km/h', '20-25 km/h', '25-30 km/h', '30-35 km/h', '35-40 km/h','40-45 km/h')
+    if location == "sb":
+        medidas = ('0-5 km/h', '5-10 km/h', '10-15 km/h', '15-20 km/h', '20-25 km/h', '25-30 km/h', '30-35 km/h', '35-40 km/h','40-45 km/h')
+    elif location == 'ps':
+        medidas = ('0-10 km/h', '10-20 km/h', '20-30 km/h', '30-40 km/h', '40-50 km/h', '50-60 km/h', '60-70 km/h', '70-80 km/h', '80-90 km/h')
     category_orders={'direcao_cat': direcoes, 'velocidade_cat': medidas}
     
     if kind == 'chuva':
